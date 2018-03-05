c1b9f7616ed902660670edc3e0a1a0b0a4e27181
p2bilt <peteralan@gmail.com>
2018-03-04 21:49:01 -0500

    second commit

--- listener.js
+++ listener.js
@@ -1,5 +1,5 @@
 const app = require('./app/server.js');
-const port = process.env.PORT || process.argv[2] || 80;
+const port = process.env.PORT || process.argv[2] || 8080;
 
 app.listen(port, function() { 
         console.log(`A long time ago, on port ${port}...`); 
